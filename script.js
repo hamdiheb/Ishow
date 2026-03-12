dbef8542ec629601c438c52d7d52d243cecd1dfe
Iheb Hamdi <67348203+hamdiheb@users.noreply.github.com>
2026-03-12 12:29:27 +0100

    Implemented function that render shows on the main

---
+++ script.js
@@ -0,0 +1,42 @@
+async function fetchShows() {
+  const query = await fetch('https://api.tvmaze.com/shows?page=1')
+  const respond = await query.json()
+  return respond
+}
+
+function createShowComponent(show) {
+  const showArticle = document.createElement('article')
+
+  const showTitle = document.createElement('h3')
+  showTitle.textContent = show.name
+  const showImg = document.createElement('img')
+  showImg.src = show.image.medium
+  showImg.classList.add('show_img')
+  const showDate = document.createElement('p')
+  showDate.textContent = show.premiered
+  const showDescription = document.createElement('p')
+  showDescription.innerHTML = show.summary
+
+  const showData = document.createElement('div')
+  showData.append(showTitle, showDate, showDescription)
+  showData.classList.add('show_component_data')
+  showArticle.append(showImg, showData)
+  showArticle.classList.add('show_component')
+  return showArticle
+}
+
+async function displayShows(displayMax) {
+  const showsArray = await fetchShows()
+  const showsDisplay = document.querySelector('#main_show_display')
+
+  for (let i = 0; i < displayMax; i++) {
+    const showComponent = createShowComponent(showsArray[i])
+    showsDisplay.appendChild(showComponent)
+  }
+}
+function init(displayMax) {
+  displayShows(displayMax)
+}
+
+const displayMax = 24
+init(displayMax)
